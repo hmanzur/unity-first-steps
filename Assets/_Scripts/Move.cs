1fea0dfad4c7e3a3af040e9d6df09467fd28d89f
Habib Manzur <habibmanzur@outlook.com>
2020-09-12 13:08:50 -0500

    Adding Assets and Scripts

---
+++ Assets/_Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    float x;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // if (Input.GetKeyDown(KeyCode.LEFT) this.tranform.Translate(-X, 0, 0);
+        // else if Input.GetKeyDown(KeyCode.RIGHT) this.tranform.Translate(-X, 0, 0);
+
+        x = Input.GetAxis("Horizontal");
+
+        //if (x)
+        //{
+        this.transform.Translate(x * 0.5f, 0, 0);
+        //}
+        
+    }
+}
